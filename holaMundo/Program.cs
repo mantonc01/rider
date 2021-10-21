da97dcc4a8c44b846a76e19b822824868924af6f
mantonc01 <91096244+mantonc01@users.noreply.github.com>
2021-10-21 16:18:11 +0200

    Primer commit de Rider

---
+++ holaMundo/Program.cs
@@ -0,0 +1,12 @@
+﻿    using System;
+    using System.Threading;
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+        Console.WriteLine("Hola Mundo!!!!!!");
+        Thread.Sleep(30000);
+        
+        }
+    }
