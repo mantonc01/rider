a61a56f543bb933661d398b33d00ce83c60eeb79
mantonc01 <91096244+mantonc01@users.noreply.github.com>
2021-10-22 19:54:13 +0200

    casteo de numeros

--- holaMundo/Program.cs
+++ holaMundo/Program.cs
@@ -40,7 +40,7 @@
         }
     }
     */
-    public class Program
+    /*public class Program
     {
         public static void Main(string[] args)
         {
@@ -66,6 +66,145 @@
             else
             {
                 Console.WriteLine("tiene 0 cifras");
+            }
+        }
+    }*/
+    /*public class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            /*(2.2.2.4) Crea un programa que pida al usuario su identificador y su contraseña
+                (ambos numéricos), y no le permita seguir hasta que introduzca como
+                identificador "1234" y como contraseña "1111".#1#
+            
+            int identificador=1234,contraseña=1111,identifica,contra;
+            Console.WriteLine("introduce tu identificador y contraseña : ");
+            
+
+            do
+            {
+               Console.Write("identificador: ");
+                           identifica = Convert.ToInt32(Console.ReadLine());
+                Console.Write("contraseña: ");
+                           contra = Convert.ToInt32(Console.ReadLine());
+                           if ((identifica!=identificador)&(contra!=contraseña))
+                           {
+                               Console.WriteLine("incorrecto");
+                           }
+                           else
+                           {
+                               Console.WriteLine("correcto");
+                           }
+            } while ((identifica!=identificador)&(contra!=contraseña));
+            
+        }
+    }*/
+    /*public class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            /*(2.2.3.1.3) Crea un programa que muestre los números del 100 al 200 (ambos
+            incluidos) que sean divisibles entre 7 y a la vez entre 3.#1#
+
+            int identificador = 1234, contraseña = 1111, identifica, contra;
+            Console.WriteLine("introduce tu identificador y contraseña : ");
+            Console.WriteLine("numeros divisibles entre 7 y 3");
+            for (int i = 100; i <= 200; i++)
+            {
+                if ((i % 7 == 0) & (i % 3 == 0))
+                {
+                    Console.WriteLine(i);
+                }
+
+            }
+
+
+
+        }
+    }*/
+    /*public class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            /*(2.2.5.3) Crea un programa que, para los números entre el 10 y el 20 (ambos
+            incluidos) diga si son divisibles entre 5, si son divisibles entre 6 y si son divisibles
+            entre 7.#1#
+
+            int identificador = 1234, contraseña = 1111, identifica, contra;
+            Console.WriteLine("introduce tu identificador y contraseña : ");
+            Console.WriteLine("numeros divisibles entre 5 , 6, y 7");
+            for (int i = 10; i <= 20; i++)
+            {
+                for (int j = 5; j <= 7; j++)
+                {
+                    if ((i % j == 0))
+                    {
+                        Console.WriteLine(i + " es divisible entre {0}",j);
+                    }
+                }
+            }
+        }
+    }*/
+    /*public class Program
+    { public static void Main(string[] args)
+        {
+            /*(2.7.2) Crea un programa que pregunte al usuario su edad y su año de nacimiento.
+            Si la edad que introduce no es un número válido, mostrará un mensaje de aviso,
+            pero aun así le preguntará su año de nacimiento.#1#
+            int age, year;
+            try
+            {
+                Console.Write("introduce tu edad : ");
+                age = Convert.ToInt32(Console.ReadLine());
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine(" Has introducido un texto en la edad");
+            }
+            finally
+            {
+                try
+                {
+                    Console.Write("introduce tu año de nacimiento : ");
+                    year = Convert.ToInt32(Console.ReadLine());
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine(" Has introducido un texto en el año");
+                }
+            }
+        }
+    }*/
+    public class Program
+    { public static void Main(string[] args)
+        {
+            /*(2.7.2) Crea un programa que pregunte al usuario su edad y su año de nacimiento.
+            Si la edad que introduce no es un número válido, mostrará un mensaje de aviso,
+            pero aun así le preguntará su año de nacimiento.*/
+            int age, year;
+            try
+            {
+                Console.Write("introduce tu edad : ");
+                age = Convert.ToInt32(Console.ReadLine());
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine(" Has introducido un texto en la edad");
+            }
+            finally
+            {
+                try
+                {
+                    Console.Write("introduce tu año de nacimiento : ");
+                    year = Convert.ToInt32(Console.ReadLine());
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine(" Has introducido un texto en el año");
+                }
             }
         }
     }
